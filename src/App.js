fcd39e220e35bbeed0fde5fbf2ae1ecab2788de4
harikrishnan-13 <135119257+harikrishnan-13@users.noreply.github.com>
2024-02-06 18:25:13 +0530

    form added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import AddPostForm from './features/posts/AddPostForm';
 import PostsList from './features/posts/PostsList';
 
 function App() {
   return (
     <main>
+      <AddPostForm />
       <PostsList />
     </main>
   );
