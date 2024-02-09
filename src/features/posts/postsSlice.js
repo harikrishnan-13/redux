c300f4d064cb54366615fbdd569d9beac978b0e5
harikrishnan-13 <135119257+harikrishnan-13@users.noreply.github.com>
2024-02-09 14:07:44 +0530

    lesson-1 redux completed successfully

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,15 +1,32 @@
-import { createSlice,nanoid } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from 'date-fns';
 
 const initialState = [
     {
         id: '1',
-        title: 'Learnind Redux toolkit',
-        content: "its easy"
+        title: 'Learning Redux Toolkit',
+        content: "I've heard good things.",
+        date: sub(new Date(), { minutes: 10 }).toISOString(),
+        reactions: {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0
+        }
     },
     {
         id: '2',
-        title: 'Subcribe',
-        content: "Like and Share this Video"
+        title: 'Slices...',
+        content: "The more I say slice, the more I want pizza.",
+        date: sub(new Date(), { minutes: 5 }).toISOString(),
+        reactions: {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0
+        }
     }
 ]
 
@@ -26,14 +43,32 @@ const postsSlice = createSlice({
                     payload: {
                         id: nanoid(),
                         title,
-                        content
+                        content,
+                        date: new Date().toISOString(),
+                        userId,
+                        reactions: {
+                            thumbsUp: 0,
+                            wow: 0,
+                            heart: 0,
+                            rocket: 0,
+                            coffee: 0
+                        }
                     }
                 }
+            }
+        },
+        reactionAdded(state, action) {
+            const { postId, reaction } = action.payload
+            const existingPost = state.find(post => post.id === postId)
+            if (existingPost) {
+                existingPost.reactions[reaction]++
             }
         }
     }
 })
 
-export const { postAdded } = postsSlice.actions;
 export const selectAllPosts = (state) => state.posts;
+
+export const { postAdded, reactionAdded } = postsSlice.actions
+
 export default postsSlice.reducer
